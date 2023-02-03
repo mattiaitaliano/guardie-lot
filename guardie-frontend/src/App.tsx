0fa59a6c979841988e26a6ccab33921dde31c0b5
mattiaitaliano <mattia94italiano@gmail.com>
2023-02-03 14:01:46 +0000

    Adds routing

--- guardie-frontend/src/App.tsx
+++ guardie-frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./bootstrap.min.css";
+import "./App.css";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      ciao
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
--- guardie-frontend/src/components/Header.tsx
+++ guardie-frontend/src/components/Header.tsx
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import { Container, Row, Nav, Navbar } from "react-bootstrap";
+import logo from "./media/utility/vessillo.png";
 
 export class Header extends Component {
   render() {
-    return <div></div>;
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <img
+            src={logo}
+            width="30"
+            height="40"
+            className="d-inline-block align-top"
+          />
+          <Navbar.Brand href="/">&nbsp;Legislatore 4.0</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/upload">
+                <i className="fa-solid fa-clipboard"></i> Upload
+              </Nav.Link>
+              <Nav.Link href="/resoconti">
+                <i className="fa-solid fa-pencil"></i> Resoconti
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
   }
 }
 
